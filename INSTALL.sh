792b2cb79de3f0931be76d6186c1e9cb6f0903f9
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-10-13 12:01:45 +0200

    Create INSTALL.sh

---
+++ INSTALL.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+    echo "You must run this installation script as root."
+    exit 1
+fi
+
+INSTALL_DIR="/usr/local/cpanel/base/frontend/jupiter"
+
+if [ ! -d "$INSTALL_DIR" ]; then
+    echo "Installation dir not found: $INSTALL_DIR"
+    exit 1
+fi
+
+mkdir -p $INSTALL_DIR/ai-assistant/
+cp ai-assistant.svg index.live.php install.json $INSTALL_DIR/ai-assistant/
+/usr/local/cpanel/scripts/install_plugin $INSTALL_DIR/ai-assistant/
+
+echo "DONE"
+echo
